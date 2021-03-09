6fcd927c76961c98eb189bb223c642ff37a66be4
kabelo masemola <kabelo.masemola@sambe.co.za>
2021-03-09 03:36:46 +0200

    another hound test

---
+++ client.py
@@ -0,0 +1,19 @@
+from confo.Backends.FileBackend import FileBackend
+
+# fb = FileBackend()
+# cred = {"config_path": "/home/kabelo/workspace/sambe_internship/Configurations/"}
+# fb.set_system(system_name="systemB")
+# fb.load_credentials(credentials=cred)
+# # print(fb.get_all())
+# # fb.set("trello","credentials",{"name":"kabelo"})
+# print(fb.get("trello"))
+import os
+
+
+
+
+import time
+time.sleep(10)
+shutil.rmtree(mock)
+
+
